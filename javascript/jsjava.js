f1f43ef65a04775354541e68dec232e4e2db3dc6
Thomas Keller <thomas_keller@gmx.ch>
2014-05-23 10:32:13 +0200

    a

--- javascript/jsjava.js
+++ javascript/jsjava.js
@@ -7,7 +7,7 @@ var options = {
 function success(pos) {
   var crd = pos.coords;
 
-  $('.js-lat').text(crd.latitude);
+  $('.js-lt').text(crd.latitude);
   $('.js-long').text(crd.longitude);
   $('.js-acc').text(crd.accuracy);
 
