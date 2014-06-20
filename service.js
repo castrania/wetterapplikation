2a0a8938c3dcbefefc9cca82487ceca627b7d487
Thomas Keller <thomas_keller@gmx.ch>
2014-06-20 09:17:47 +0200

    as

--- service.js
+++ service.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		enableHeigAccuracy: true,
 		timeout: 5000,
 		maximumAge: 0
-		
+
 	};
 
 	var success = function(pos) {
@@ -27,29 +27,13 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 
 // http://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true_or_false
 
-$('.js-current-adresse').on('click', 'a', function(event) {
-	event.preventDefault();
-
-	var address = $('input', '.js-current-adresse').val();
-
-	$.ajax({
-		url: 'http://maps.googleapis.com/maps/api/geocode/json',
+$.ajax({
+		url: 'https://maps.googleapis.com/maps/api/geocode/json',
 		data: {
-			address: address,
-			sensor: false
+			latlng: crd.latitude + ',' + crd.longitude,
+			sensor: true
 		},
 		success: function(data) {
-			$('.js-custom-address-result').text(
-				data.results[0].geometry.location.lat +
-				',' +
-				data.results[0].geometry.location.lng
-			);
-
-			getWeahterData(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng, function(data) {
-				$('.js-custom-address-temp').text(data.currently.apparentTemperature + ' °C');
-
-				$('.js-custom-weather-icon').text(weatherIcons[data.currently.icon]);
-			});
+			$('.js-current-adresse').text(data.results[0].formatted_address);
 		}
 	});
-});
