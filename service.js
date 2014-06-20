46d67c940bc9649adb4d74850ed5d87708c1ca7b
Thomas Keller <thomas_keller@gmx.ch>
2014-06-20 09:39:16 +0200

    as

--- service.js
+++ service.js
@@ -1,39 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	var options = {
-		enableHeigAccuracy: true,
+		enableHighAccuracy: true,
 		timeout: 5000,
 		maximumAge: 0
-
 	};
 
 	var success = function(pos) {
 		var crd = pos.coords;
 
+		$.ajax({
+			url: 'https://maps.googleapis.com/maps/api/geocode/json',
+			data: {
+				latlng: crd.latitude + ',' + crd.longitude,
+				sensor: true,
+				language: 'ar'
+			},
+			success: function(data) {
+				$('.js-current-address').text(data.results[0].formatted_address);
+			}
+		});
+
 		$('.js-current-position').text(crd.latitude + ', ' + crd.longitude);
 	};
 
-	var error = function() {
-		console.warn('ERROR(' + err.code + '):' + error.message):
-	}:
+	var error = function(err) {
+		console.warn('ERROR(' + err.code + '): ' + err.message);
+	};
 
 	navigator.geolocation.getCurrentPosition(success, error, options);
 });
-
-function error(err) {
-	console.warn('ERROR(' + err.code + '): ' + err.message);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);
-
-// http://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true_or_false
-
-$.ajax({
-		url: 'https://maps.googleapis.com/maps/api/geocode/json',
-		data: {
-			latlng: crd.latitude + ',' + crd.longitude,
-			sensor: true
-		},
-		success: function(data) {
-			$('.js-current-adresse').text(data.results[0].formatted_address);
-		}
-	});
