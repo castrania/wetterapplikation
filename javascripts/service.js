d0300c61e8929619c9261871d96db86172b9691d
Thomas Keller <thomas_keller@gmx.ch>
2014-06-20 11:42:01 +0200

    asd

---
+++ javascripts/service.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0
+	};
+
+	var success = function(pos) {
+		var crd = pos.coords;
+
+		$.ajax({
+			url: 'https://maps.googleapis.com/maps/api/geocode/json',
+			data: {
+				latlng: crd.latitude + ',' + crd.longitude,
+				sensor: true,
+				language: 'ar'
+			},
+			success: function(data) {
+				$('.js-current-address').text(data.results[0].formatted_address);
+			}
+		});
+
+		$('.js-current-position').text(crd.latitude + ', ' + crd.longitude);
+	};
+
+	var error = function(err) {
+		console.warn('ERROR(' + err.code + '): ' + err.message);
+	};
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
+
+	$(document).on('change', '. js-language',function()){
+		console.log()
+	}
+});
